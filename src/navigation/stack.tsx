88a50f258f3f12f5c6edce8f768003e07f323c2d
PhiHung160420 <hungphanlk@gmail.com>
2022-03-31 18:02:57 +0700

    Done Project

--- src/navigation/stack.tsx
+++ src/navigation/stack.tsx
@@ -8,8 +8,8 @@ const Stack = createStackNavigator();
 export const RootStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false}}>
-      <Stack.Screen name={screenNames.LAYOUT_SCREEN} component={LayoutScreen} />
       <Stack.Screen name={screenNames.LANDING_SCREEN} component={LandingScreen} />
+      <Stack.Screen name={screenNames.LAYOUT_SCREEN} component={LayoutScreen} />
       <Stack.Screen name={screenNames.COURSE_LISTING_SCREEN} component={CourseListingScreen} />
       <Stack.Screen name={screenNames.COURSE_DETAIL_SCREEN} component={CourseDetailScreen} />
       <Stack.Screen name={screenNames.LOGIN_SCREEN} component={LoginScreen} />
