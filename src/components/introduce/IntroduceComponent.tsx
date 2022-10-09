fc424776e799f84bd39e352b93bb5aa6bebd6cb8
phihung160420 <hungphanlk@gmail.com>
2022-10-09 22:05:19 +0700

    add introduce component

---
+++ src/components/introduce/IntroduceComponent.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const IntroduceComponent = () => {
+  return (
+    <View>
+      <Text>IntroduceComponent</Text>
+    </View>
+  );
+};
+
+export default IntroduceComponent;
+
+const styles = StyleSheet.create({});
--- src/screens/introduce/IntroduceScreen.tsx
+++ src/screens/introduce/IntroduceScreen.tsx
@@ -1,4 +1,4 @@
-import {View, ViewComponent} from 'react-native';
+import {View} from 'react-native';
 
 const IntroduceScreen = () => {
   return <View />;
